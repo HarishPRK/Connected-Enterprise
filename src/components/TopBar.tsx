3ee543b293d0f77e71102f643ff4b81b8993b488
HarishPRK <hradhakrishnan66@gmail.com>
2026-09-01 14:38:40 -0500

    chore: restore ANIRA 2.0 branding

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -32,7 +32,7 @@ export function TopBar({
           <img src="/capgemini.jpg" alt="Capgemini" />
         </span>
         <span>
-          Connected Enterprise
+          Connected Enterprise / ANIRA 2.0
           <small>Cloud operations</small>
         </span>
       </div>
